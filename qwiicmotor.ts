b0259aa3c0acb080b32e018058675eae59755085
calliope-net <141074597+calliope-net@users.noreply.github.com>
2024-06-09 14:05:22 +0200

    Aktualisiere qwiicmotor.ts

--- qwiicmotor.ts
+++ qwiicmotor.ts
@@ -29,58 +29,73 @@ namespace kran_e { // qwiicmotor.ts
     const CONTROL_1 = 0x78 // 0x01: Reset the processor now.
 
     const c_MotorStop = 128
-    let n_MotorChipABReady = false
-    let n_MotorChipCDReady = false
+    //let n_MotorChipABReady = false
+    //let n_MotorChipCDReady = false
     let n_MotorChipReady = [false, false, false, false]
     let n_MotorPower = [false, false, false, false]
     let n_MotorSpeed = [c_MotorStop, c_MotorStop, c_MotorStop, c_MotorStop]
-   // let n_MotorON = false       // aktueller Wert im Chip
-  //  let n_MotorA = c_MotorStop  // aktueller Wert im Chip
+    // let n_MotorON = false       // aktueller Wert im Chip
+    //  let n_MotorA = c_MotorStop  // aktueller Wert im Chip
 
     // group="Motor"
     // block="Motor Reset %i2cMotor" weight=9
     export function motorReset(i2cMotor: ei2cMotor) {
-        n_MotorChipABReady = false
+        switch (i2cMotor) {
+            case ei2cMotor.i2cMotorAB: {
+                n_MotorChipReady[eMotor.ma] = false
+                n_MotorChipReady[eMotor.mb] = false
+            }
+            case ei2cMotor.i2cMotorCD: {
+                n_MotorChipReady[eMotor.mc] = false
+                n_MotorChipReady[eMotor.md] = false
+            }
+        }
         if (pins.i2cWriteBuffer(i2cMotor, Buffer.fromArray([ID]), true) != 0) {
-            addStatus(i2cMotor)
+            addStatusHEX(i2cMotor)
             return false
         } else if (pins.i2cReadBuffer(i2cMotor, 1).getUint8(0) == 0xA9) { // Reports hard-coded ID byte of 0xA9
             pins.i2cWriteBuffer(i2cMotor, Buffer.fromArray([CONTROL_1, 1])) // Reset the processor now.
             return true
         } else
-            addStatus(i2cMotor + "A9")
+            addStatusHEX(0x10 + i2cMotor)
         return false
     }
 
-    // group="Motor"
-    // block="Motor bereit %i2cMotor" weight=8
-     function motorStatus(i2cMotor: ei2cMotor): boolean {
-        if (n_MotorChipABReady)
+    function motorChipReady(pMotor: eMotor) {
+        if (n_MotorChipReady[pMotor])
             return true
-        /*
-        bool ready( void );
-        This function checks to see if the SCMD is done booting and is ready to receive commands. Use this
-        after .begin(), and don't progress to your main program until this returns true.
-        SCMD_STATUS_1: Read back basic program status
-            B0: 1 = Enumeration Complete
-            B1: 1 = Device busy
-            B2: 1 = Remote read in progress
-            B3: 1 = Remote write in progress
-            B4: Read state of enable pin U2.5"
-        */
         else {
-            for (let i = 0; i < 5; i += 1) {
-                pause(2000) // 2 s lange Wartezeit
-                pins.i2cWriteBuffer(i2cMotor, Buffer.fromArray([STATUS_1]), true)
-
-                if ((pins.i2cReadBuffer(i2cMotor, 1).getUint8(0) & 0x01) == 1) { // STATUS_1
-                    n_MotorChipABReady = true
+            let i2cMotor: ei2cMotor
+            switch (pMotor) {
+                case eMotor.ma, eMotor.mb: {
+                    i2cMotor = ei2cMotor.i2cMotorAB
                     break
                 }
-            }
-            return n_MotorChipABReady
-        }
+                case eMotor.mc, eMotor.md: {
+                    i2cMotor = ei2cMotor.i2cMotorCD
+                    break
+                }
+            } // switch
+            /*
+            bool ready( void );
+            This function checks to see if the SCMD is done booting and is ready to receive commands. Use this
+            after .begin(), and don't progress to your main program until this returns true.
+            SCMD_STATUS_1: Read back basic program status
+                B0: 1 = Enumeration Complete
+                B1: 1 = Device busy
+                B2: 1 = Remote read in progress
+                B3: 1 = Remote write in progress
+                B4: Read state of enable pin U2.5"
+            */
+            n_MotorChipReady[pMotor] = (pins.i2cWriteBuffer(i2cMotor, Buffer.fromArray([STATUS_1]), true) == 0
+                && (pins.i2cReadBuffer(i2cMotor, 1).getUint8(0) & 0x01) == 1) // STATUS_1
+
+            return n_MotorChipReady[pMotor]
+        } // else
     }
+
+
+   
 
 
 } // qwiicmotor.ts
