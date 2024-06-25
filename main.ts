e5a3f7324f962c145a426b6c74be1eb100222507
calliope-net <141074597+calliope-net@users.noreply.github.com>
2024-06-25 20:48:03 +0200

    Aktualisiere pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -12,23 +12,17 @@ radio.onReceivedData(function (receivedData) {
         receiver.qMotor255(receiver.eMotor.mc, radio.getByte(receivedData, radio.eBufferPointer.mc, radio.eBufferOffset.b0_Motor))
         receiver.ringTone(radio.getSchalter(receivedData, radio.e0Schalter.b0))
         receiver.qwiicRelay(radio.getSchalter(receivedData, radio.e0Schalter.b1))
-        receiver.rgbLEDs(receiver.eRGBled.a, 0xff0000, true)
+        receiver.rgbLEDs(receiver.eRGBled.a, 0x0000ff, true)
     }
     if (receiver.chStatus() && radio.getSchalter(receivedData, radio.e0Schalter.b6)) {
         radio.sendString(receiver.getStatus(true))
     }
+    radio.zeige5x5Status(receivedData)
 })
 receiver.beimStart(90)
-basic.showLeds(`
-    . . . . #
-    . . # # .
-    . . # . .
-    # # # # #
-    . # . # .
-    `)
 loops.everyInterval(700, function () {
     if (radio.timeout(1000)) {
-        receiver.rgbLEDs(receiver.eRGBled.a, 0x00ff00, true)
+        receiver.rgbLEDs(receiver.eRGBled.a, 0x00ff00, true, 5)
         receiver.qMotorChipPower(receiver.eMotorChip.ab, false)
         receiver.qMotorChipPower(receiver.eMotorChip.cd, false)
     }
