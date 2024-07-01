d218336aeb664b799d07f8db3bd4932eaa7c4fac
calliope-net <141074597+calliope-net@users.noreply.github.com>
2024-07-01 15:29:26 +0200

    Aktualisiere pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,15 +1,19 @@
+function macheEtwas (receivedData: any[]) {
+    receiver.motor255(receiver.eMotor01.M0, radio.getByte(null, radio.eBufferPointer.m0, radio.eBufferOffset.b0_Motor))
+    receiver.servo_set16(radio.getByte(null, radio.eBufferPointer.m0, radio.eBufferOffset.b1_Servo))
+    receiver.motor255(receiver.eMotor01.M1, radio.getByte(null, radio.eBufferPointer.m1, radio.eBufferOffset.b0_Motor))
+    receiver.qMotorChipPower(receiver.eMotorChip.ab, radio.getaktiviert(null, radio.e3aktiviert.ma) || radio.getaktiviert(null, radio.e3aktiviert.mb))
+    receiver.qMotor255(receiver.eMotor.ma, radio.getByte(null, radio.eBufferPointer.ma, radio.eBufferOffset.b0_Motor))
+    receiver.qMotor255(receiver.eMotor.mb, radio.getByte(null, radio.eBufferPointer.mb, radio.eBufferOffset.b0_Motor))
+    receiver.qMotorChipPower(receiver.eMotorChip.cd, radio.getaktiviert(null, radio.e3aktiviert.mc))
+    receiver.qMotor255(receiver.eMotor.mc, radio.getByte(null, radio.eBufferPointer.mc, radio.eBufferOffset.b0_Motor))
+    receiver.qMotor255(receiver.eMotor.md, radio.getByte(null, radio.eBufferPointer.md, radio.eBufferOffset.b0_Motor))
+}
 radio.onReceivedData(function (receivedData) {
     if (radio.isBetriebsart(receivedData, radio.e0Betriebsart.p0)) {
-        receiver.motor255(receiver.eMotor01.M0, radio.getByte(receivedData, radio.eBufferPointer.m0, radio.eBufferOffset.b0_Motor))
-        receiver.servo_set16(radio.getByte(receivedData, radio.eBufferPointer.m0, radio.eBufferOffset.b1_Servo))
-        receiver.motor255(receiver.eMotor01.M1, radio.getByte(receivedData, radio.eBufferPointer.m1, radio.eBufferOffset.b0_Motor))
-        receiver.qMotorChipPower(receiver.eMotorChip.ab, radio.getaktiviert(receivedData, radio.e3aktiviert.ma) || radio.getaktiviert(receivedData, radio.e3aktiviert.mb))
-        receiver.qMotor255(receiver.eMotor.ma, radio.getByte(receivedData, radio.eBufferPointer.ma, radio.eBufferOffset.b0_Motor))
-        receiver.qMotor255(receiver.eMotor.mb, radio.getByte(receivedData, radio.eBufferPointer.mb, radio.eBufferOffset.b0_Motor))
-        receiver.qMotorChipPower(receiver.eMotorChip.cd, radio.getaktiviert(receivedData, radio.e3aktiviert.mc))
-        receiver.qMotor255(receiver.eMotor.mc, radio.getByte(receivedData, radio.eBufferPointer.mc, radio.eBufferOffset.b0_Motor))
-        receiver.qMotor255(receiver.eMotor.md, radio.getByte(receivedData, radio.eBufferPointer.md, radio.eBufferOffset.b0_Motor))
+        receiver.sendM0(receivedData)
         receiver.ringTone(radio.getSchalter(receivedData, radio.e0Schalter.b0))
+        receiver.digitalWritePin(receiver.eDigitalPins.C16, !(radio.getSchalter(receivedData, radio.e0Schalter.b0)))
         receiver.qwiicRelay(radio.getSchalter(receivedData, radio.e0Schalter.b1))
         receiver.pinGPIO4(radio.getSchalter(receivedData, radio.e0Schalter.b2))
         receiver.rgbLEDs(receiver.eRGBled.a, 0x0000ff, true)
