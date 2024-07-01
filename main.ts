22a5f764ee2793e388874a244ca741aa78839cb1
calliope-net <141074597+calliope-net@users.noreply.github.com>
2024-07-01 20:02:17 +0200

    Aktualisiere pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,4 +1,4 @@
-function macheEtwas (receivedData: any[]) {
+function deaktiviert (receivedData: any[]) {
     receiver.motor255(receiver.eMotor01.M0, radio.getByte(null, radio.eBufferPointer.m0, radio.eBufferOffset.b0_Motor))
     receiver.servo_set16(radio.getByte(null, radio.eBufferPointer.m0, radio.eBufferOffset.b1_Servo))
     receiver.motor255(receiver.eMotor01.M1, radio.getByte(null, radio.eBufferPointer.m1, radio.eBufferOffset.b0_Motor))
@@ -34,11 +34,15 @@ input.onButtonEvent(Button.B, input.buttonEventValue(ButtonEvent.Hold), function
 })
 receiver.beimStart(receiver.eModell.v3, 90, storage.getNumber(StorageSlots.s1))
 storage.putNumber(StorageSlots.s1, radio.storageBufferGet())
+lcd20x4.initLCD(lcd20x4.lcd20x4_eADDR(lcd20x4.eADDR.LCD_20x4))
+lcd20x4.writeText(lcd20x4.lcd20x4_eADDR(lcd20x4.eADDR.LCD_20x4), 0, 0, 19, lcd20x4.lcd20x4_text("Calliope mini v3"))
+lcd20x4.writeText(lcd20x4.lcd20x4_eADDR(lcd20x4.eADDR.LCD_20x4), 1, 0, 19, lcd20x4.lcd20x4_text("Maker Kit Car"))
 loops.everyInterval(700, function () {
     if (radio.timeout(60000, true)) {
         receiver.pinRelay(false)
     } else if (radio.timeout(1000)) {
         receiver.rgbLEDs(receiver.eRGBled.a, 0x00ff00, true, 5)
+        receiver.ringTone(false)
         receiver.qMotorChipPower(receiver.eMotorChip.ab, false)
         receiver.qMotorChipPower(receiver.eMotorChip.cd, false)
     }
